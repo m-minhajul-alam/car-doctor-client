40a7003f120da30d1324b0de7da70034d217f727
Minhajul-alam <alam.minhajul@outlook.com>
2023-10-24 18:28:47 +0600

    Added navbar

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/shered/footer/footer";
+import Navbar from "../pages/shered/navbar/Navbar";
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
---
+++ src/pages/shered/navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.svg"
+
+const Navbar = () => {
+    const navLinks = <>
+        <Link to={'/'}><li>Home</li></Link>
+        <Link to={'/about'}><li>About</li></Link>
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navLinks}
+                    </ul>
+                </div>
+                <Link><img className="w-14" title="car doctor" src={logo} alt="" /></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 gap-5">
+                    {navLinks}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
