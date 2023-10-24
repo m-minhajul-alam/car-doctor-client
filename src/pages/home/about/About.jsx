9b76f7f00638629258ca5098deb0998e537e74e9
Minhajul-alam <alam.minhajul@outlook.com>
2023-10-24 20:52:58 +0600

    added about us section:

---
+++ src/pages/home/about/About.jsx
@@ -0,0 +1,23 @@
+import person from "../../../assets/images/about_us/person.jpg"
+import parts from "../../../assets/images/about_us/parts.jpg"
+
+const About = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="lg:flex-1 relative">
+                    <img src={person} className="w-96 rounded-lg shadow-2xl" />
+                    <img src={parts} className="absolute right-36 border-8 border-white top-2/3  w-60 rounded-lg shadow-2xl" />
+                </div>
+                <div className="lg:flex-1">
+                    <p className="text-orange-500 font-bold">About Us</p>
+                    <h1 className="text-5xl font-bold">We are qualified <br /> & of experience <br /> in this field</h1>
+                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+                    <p className="py-6">the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+                    <button className="btn btn-error">Get More Info</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default About;
--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,9 +1,11 @@
+import About from "../about/About";
 import Banner from "../banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <About></About>
         </div>
     );
 };
