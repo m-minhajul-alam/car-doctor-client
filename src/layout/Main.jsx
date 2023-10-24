5b809bf4ba4b40244388a3d6ec2360d15f0d1998
Minhajul-alam <alam.minhajul@outlook.com>
2023-10-24 17:17:40 +0600

    Added footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/shered/footer/footer";
 
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
-
+            <Footer></Footer>
         </div>
     );
 };
